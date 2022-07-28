5e3097ae9a33ffb2eba640d8be854091219219e0
Omer Hazan <omerathome@gmail.com>
2022-07-28 17:05:46 +0200

    back without build in url

--- wdio.conf.js
+++ wdio.conf.js
@@ -104,7 +104,7 @@ exports.config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl: "http://web/build", // Local
+  baseUrl: "http://web", // Local
   // baseUrl: "https://main--mathias-hazan-lira.netlify.app/", // Live
 
   //
